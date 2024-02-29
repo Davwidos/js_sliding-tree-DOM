0836de24aa519545a74d8789a89d688b88a59449
Davwidos <d.jurkowski015@gmail.com>
2024-02-29 15:13:35 +0100

    make build

---
+++ index.359a01ba.js
@@ -0,0 +1,4 @@
+var e=document.querySelector(".tree");!function(e){e.querySelectorAll("ul").forEach(function(e){var n=e.previousSibling,t=document.createElement("SPAN");t.append(n),e.before(t)})}(e),e.addEventListener("click",// write code here
+function(e){var n=e.target;if("SPAN"===n.tagName){var t=n.nextSibling,l=t.style.display;t.style.display="none"===l?null:"none"}});//# sourceMappingURL=index.359a01ba.js.map
+
+//# sourceMappingURL=index.359a01ba.js.map
